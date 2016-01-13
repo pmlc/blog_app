28a551fe2c707f3c0d78c0173c67cca6bc04079f
“pmlc” <pmlc@iinet.net.au>
2016-01-13 05:27:21 +0000

    Article model - add validation

--- blog_app/spec/features/creating_article_spec.rb
+++ blog_app/spec/features/creating_article_spec.rb
@@ -13,4 +13,18 @@ RSpec.feature "Creating Articles" do
     expect(page).to have_content("Your article was created succesfully")
     expect(page.current_path).to eq(articles_path)
   end
+  
+  scenario "A user fails to create a new article" do
+    visit "/"
+    
+    click_link "New Article"
+    
+    fill_in "Title", with: ""
+    fill_in "Body", with: ""
+    click_button "Create Article" 
+    
+    expect(page).to have_content("2 errors prohibited this article from being saved")
+    expect(page).to have_content("Title can't be blank")
+    expect(page).to have_content("Body can't be blank")
+  end
 end
