c52037770e98c4587f851136a307cd746d8af18c
“pmlc” <pmlc@iinet.net.au>
2016-01-14 03:26:36 +0000

    List the articles

---
+++ blog_app/spec/features/listing_articles_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.feature "Listing Articles" do
+
+  before do
+    @article1 = Article.create(title: "the first title", body: "body of first article")
+    @article2 = Article.create(title: "the second title", body: "body of second article")
+  end
+  
+  scenario "list all articles" do
+    visit "/"
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+  end
+  
+end
+
