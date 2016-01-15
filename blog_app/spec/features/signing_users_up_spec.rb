c802553f3cd4db50220e4be05399312f9d7685cf
“pmlc” <pmlc@iinet.net.au>
2016-01-15 22:04:11 +0000

    User signup added

---
+++ blog_app/spec/features/signing_users_up_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.feature "Users signup" do
+  
+  scenario "with valid user id password" do
+    visit "/"
+    click_link "Sign up"
+    fill_in "Email", with: "pmlc@iinet.net.au"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Sign up" 
+    expect(page).to have_content("Welcome! You have signed up successfully.")
+  end
+
+end
